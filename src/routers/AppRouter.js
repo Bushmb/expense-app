4bde698a78f40bb1e0dc1318c943d30b8b8f60a0
bushmb <bushmb@gmail.com>
2018-03-14 13:39:51 -0700

    componentalized

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "../components/Header";
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
-
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
-
-const HelpPage = () => <div>This is from my help component</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404! There is nothing here! - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink exact to="/" activeClassName="is-active">
-      Home Page
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit Expense
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
+import EditExpensePage from "../components/EditExpensePage";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -38,7 +14,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
